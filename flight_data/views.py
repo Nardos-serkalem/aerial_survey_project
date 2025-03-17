3f567c3a25b006b754d6e5777dc6004ce764a60a
Nardos-serkalem <nadii8044@gmail.com>
2025-03-17 09:26:53 +0300

    update view

--- flight_data/views.py
+++ flight_data/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
-from .models import FlightData
 
 def flight_data_list(request):
     if request.method == 'POST':
-        # Retrieve data from the form
+        # Retrieve form data
         project_name = request.POST.get('project_name')
         pilot = request.POST.get('pilot')
         flight_date = request.POST.get('flight_date')
@@ -16,20 +15,20 @@ def flight_data_list(request):
         mount = request.POST.get('mount')
         imu = request.POST.get('imu')
         camera = request.POST.get('camera')
-        serial_no = request.POST.get('serial no')
-        focal_length = request.POST.get('focal length')
+        serial_no = request.POST.get('serial_no')
+        focal_length = request.POST.get('focal_length')
         gps_data_logging_time = request.POST.get('gps_data_logging_time')
-        sun_angle = request.POST.get('sun angle')
-        none = request.POST.get('none ')
+        sun_angle = request.POST.get('sun_angle')
+        none = request.POST.get('none')
         internal_pos_data_code = request.POST.get('internal_pos_data_code')
         aperture = request.POST.get('aperture')
-        shutter_speed = request.POST.get('shutter speed')
+        shutter_speed = request.POST.get('shutter_speed')
         iso = request.POST.get('iso')
         fmc = request.POST.get('fmc')
         ibd = request.POST.get('ibd')
         engine_start = request.POST.get('engine_start')
         start_movement = request.POST.get('start_movement')
-        take_off = request.POST.get('take off')
+        take_off = request.POST.get('take_off')
         landing = request.POST.get('landing')
         stop_movement = request.POST.get('stop_movement')
         shutdown = request.POST.get('shutdown')
@@ -43,44 +42,10 @@ def flight_data_list(request):
         qty = request.POST.get('qty')
         remarks = request.POST.get('remarks')
         weather = request.POST.get('weather')
-        remarks_textarea = request.POST.get('remarks')  # Corrected name
+        remarks_textarea = request.POST.get('remarks') #used for the text area.
         signature = request.POST.get('signature')
 
-        # Create a FlightData instance and save it to the database
-        flight_data = FlightData(
-            project_name=project_name,
-            pilot=pilot,
-            flight_date=flight_date,
-            flight_no_day=flight_no_day,
-            departure=departure,
-            overlap=overlap,
-            gsd=gsd,
-            aircraft=aircraft,
-            navigation_system=navigation_system,
-            mount=mount,
-            imu=imu,
-            camera=camera,
-            serial_no=serial_no,
-            focal_length=focal_length,
-            gps_data_logging_time=gps_data_logging_time,
-            sun_angle=sun_angle,
-            none=none,
-            internal_pos_data_code=internal_pos_data_code,
-            aperture=aperture,
-            shutter_speed=shutter_speed,
-            iso=iso,
-            fmc=fmc,
-            ibd=ibd,
-            engine_start=engine_start,
-            start_movement=start_movement,
-            take_off=take_off,
-            landing=landing,
-            stop_movement=stop_movement,
-            shutdown=shutdown,
-        )
-        flight_data.save()
-
-        # Create context for results.html
+        # Create a dictionary with the form data
         context = {
             'project_name': project_name,
             'pilot': pilot,
@@ -125,10 +90,8 @@ def flight_data_list(request):
             'signature': signature,
         }
 
-        # Render results.html with the context
+        # Render the results template with the data
         return render(request, 'flight_data/results.html', context)
-
     else:
-        # Display the form (GET request)
-        flight_data = FlightData.objects.all()
-        return render(request, 'flight_data/flight_data_list.html', {'flight_data': flight_data})
+        # Render the form template
+        return render(request, 'flight_data/flight_data_list.html')
