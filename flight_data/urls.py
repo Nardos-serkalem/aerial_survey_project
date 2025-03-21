0f839be9bc486758a9a9cfa964c029af22ddecb0
Nardos-serkalem <nadii8044@gmail.com>
2025-03-21 15:20:57 +0300

    add

--- flight_data/urls.py
+++ flight_data/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.flight_data_list, name='flight-data-list'),  # For the HTML page
-   
+    path('success/', lambda request: render(request, 'flight_data/success.html'), name='success'),
 ]
